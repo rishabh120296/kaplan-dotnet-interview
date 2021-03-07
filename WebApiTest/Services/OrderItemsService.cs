5c7d96dffc4bc08294f5c36488b11fb3c2c34163
Rishabh Agrawal <rishabh120296@gmail.com>
2021-03-07 23:24:34 +0530

    lint fix

--- WebApiTest/Services/OrderItemsService.cs
+++ WebApiTest/Services/OrderItemsService.cs
@@ -10,45 +10,50 @@ namespace WebApiTest.Services
 {
     public class OrderItemsService : IOrderItemsService
     {
-        public OrderItemsService( TestDbContext dbContext )
+        public OrderItemsService(TestDbContext dbContext)
         {
             testDbContext = dbContext;
         }
 
-        public OrderItemsModel Get( int orderID )
+        public OrderItemsModel Get(int orderID)
         {
-            IEnumerable<OrderItem> orderItems = testDbContext.OrderItems.Where( oi => oi.OrderID == orderID );
+            IEnumerable<OrderItem> orderItems = testDbContext.OrderItems.Where(oi => oi.OrderID == orderID);
 
-            return new OrderItemsModel
-                       {
-                           OrderID = orderID,
-                           Items = orderItems.Select( oi => new OrderItemModel
-                                                                {
-                                                                    LineNumber = oi.LineNumber,
-                                                                    ProductID = oi.ProductID,
-                                                                    StudentPersonID = oi.StudentPersonID,
-                                                                    Price = oi.Price
-                                                                } )
-                       };
-        }
-
-        public async Task<short> AddAsync( int orderID, OrderItemModel item )
-        {
-            if ( item.LineNumber != 0 )
+            if (orderItems == null)
             {
-                throw new ValidationException( "LineNumber is generated and cannot be specified" );
+                return null;
             }
 
-            var lineNumber = (short)( testDbContext.OrderItems.Where( oi => oi.OrderID == orderID ).Max( oi => oi.LineNumber ) + 1 );
+            return new OrderItemsModel
+            {
+                OrderID = orderID,
+                Items = orderItems.Select(oi => new OrderItemModel
+                {
+                    LineNumber = oi.LineNumber,
+                    ProductID = oi.ProductID,
+                    StudentPersonID = oi.StudentPersonID,
+                    Price = oi.Price
+                })
+            };
+        }
 
-            await testDbContext.OrderItems.AddAsync( new OrderItem
-                                                         {
-                                                             OrderID = orderID,
-                                                             LineNumber = lineNumber,
-                                                             Price = item.Price,
-                                                             ProductID = item.ProductID,
-                                                             StudentPersonID = item.StudentPersonID
-                                                         } );
+        public async Task<short> AddAsync(int orderID, OrderItemModel item)
+        {
+            if (item.LineNumber != 0)
+            {
+                throw new ValidationException("LineNumber is generated and cannot be specified");
+            }
+
+            var lineNumber = (short)(testDbContext.OrderItems.Where(oi => oi.OrderID == orderID).Max(oi => oi.LineNumber) + 1);
+
+            await testDbContext.OrderItems.AddAsync(new OrderItem
+            {
+                OrderID = orderID,
+                LineNumber = lineNumber,
+                Price = item.Price,
+                ProductID = item.ProductID,
+                StudentPersonID = item.StudentPersonID
+            });
 
             await testDbContext.SaveChangesAsync();
 
